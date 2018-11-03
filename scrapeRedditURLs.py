9996b8594243dafa498859e8750a20c402b1f520
Sahil Rao <sahil.rao@berkeley.edu>
2018-11-03 15:29:27 -0700

    Update scrapeRedditURLs.py
    Gets corresponding top-rated Reddit URLs based on search product.

---
+++ scrapeRedditURLs.py
@@ -0,0 +1,20 @@
+import pandas
+import urllib as ul
+from bs4 import BeautifulSoup, SoupStrainer
+import requests
+import webbrowser
+import re
+
+searchTerm = ""
+searchUrl = "https://google.com/search?q=" + ul.parse.quote_plus(searchTerm)
+response = requests.get(searchUrl)
+query = BeautifulSoup(response.text, "lxml")
+redditURLs = []
+for searchResult in query.find_all(class_ = "g"):
+    for textWithURL in searchResult.find_all("a", href=True):
+        tt = textWithURL['href']
+        indexURL = tt.find("https://www.reddit")
+        if indexURL != -1:
+            URL = tt[indexURL:]
+            redditURLs.append(URL)
+
