3f9bc8f87c6cf901e127bd81fc00ca5c53e94225
eashman123 <eashaank@gmail.com>
2018-11-03 23:03:58 -0700

    Finished Ranking and Duplicate Removal

--- Ranker.py
+++ Ranker.py
@@ -1,18 +1,33 @@
+from Reccomendation import LinkParser, Recommendation
 
-""" Reddit Recommends"""
-
-import Reccomendation
+def ranking(RecDic):
+    return rank_helper(remove_duplicates(RecDic))
 
 def remove_duplicates(RecDic): # Filters list of any reoccurung links.
-	Recs = RecDic.values
+    Recs = RecDic.values()
+    dupl = {}
+    for rec in Recs:
+        if rec.link in dupl:
+            dupl[rec.link].append(rec)
+        else:
+            dupl[rec.link] = [rec]
+    Recs = []
+    for products in dupl.values():
+        if len(products)==1:
+            Recs.extend(products)
+        else:
+            prod = products[0]
+            score = 0
+            for product in products:
+                score+=product.score
+            Recs.extend([Recommendation(prod.prod_name, score, "", prod.link, 1)])
+    return Recs
 
+def rank_helper(listance): # Ranks products according to score.
+    return sorted(listance, key = lambda rec: rec.score, reverse=True)
 
-
-def rank_products(list_of_instacnes) # Ranks products according to score.
-	pass
-
-links = ["https://www.reddit.com/r/MechanicalKeyboards/comments/7js58d/what_mechanical_keyboard_should_i_buy/",
-         "https://www.reddit.com/r/MechanicalKeyboards/comments/8ekjay/best_mechanical_keyboard_100200/",
-         "https://www.reddit.com/r/MechanicalKeyboards/comments/8lk5nh/mechanical_keyboard_suggestions/"]
-
-reccs = LinkParser(links)
+# links = ["https://www.reddit.com/r/MechanicalKeyboards/comments/7js58d/what_mechanical_keyboard_should_i_buy/",
+#          "https://www.reddit.com/r/MechanicalKeyboards/comments/8ekjay/best_mechanical_keyboard_100200/",
+#          "https://www.reddit.com/r/MechanicalKeyboards/comments/8lk5nh/mechanical_keyboard_suggestions/"]
+#
+# reccs = LinkParser(links)
