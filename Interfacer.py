cf8d5c8a587ea5421cfcd131625e797e80491a6c
eashman123 <eashaank@gmail.com>
2018-11-03 19:59:21 -0700

    Fixed Reddit Processor
    Currently missing wiki support

--- Interfacer.py
+++ Interfacer.py
@@ -1,5 +1,1 @@
-from SubmissionADT import LinksParser
-
-links = ["https://www.reddit.com/r/BudgetAudiophile/comments/7v3ea9/best_earbuds_for_under_30_on_amazon/",
-         ]
-
+from Reccomendation import LinkParser #pass in list of reddit links, returns a list of Reccomendation Instances
