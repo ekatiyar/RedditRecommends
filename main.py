9d2003e3c8f803ee04aef0c819c98ea0d7d5d807
Vishal <vishal1998@gmail.com>
2018-11-04 08:41:26 -0800

    almost done

--- main.py
+++ main.py
@@ -17,11 +17,30 @@ def search_home():
 def search_links():
     the_text = request.form['some_data']
     objs = Interfacer.search(the_text)
+    # lets = {}
+    # for i in objs:
+    #     lets.append(vars(objs))
+    # links = []
+    # for obj in objs:
+    #     json_readable = json.dumps(obj, default=lambda o: o.__dict__)
+    #     links.append(json_readable)
+    # print(links)
+    # for i in links:
+    #     print(i["prod_name"])
+    names = []
+    scores = []
     links = []
-    for obj in objs:
-        json_readable = json.dumps(obj, default=lambda o: o.__dict__)
-        links.append(json_readable)
-    return render_template("links.html", links=links)
+    for i in objs:
+        ob_dict = vars(i)
+        names.append(ob_dict["prod_name"])
+        scr = str(ob_dict["score"])
+        scores.append(scr)
+        links.append(ob_dict["link"])
+    # real_names = []
+    # for i in names:
+    #     json_readable = json.dumps(obj, default=lambda o: o.__dict__)
+    #     links.append(json_readable)
+    return render_template("links.html", links=links, names=names, scores=scores)
 
 if __name__ == "__main__":
     app.run(debug=True)
