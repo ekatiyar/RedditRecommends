5d2785ed0e245aa310987273efed0b50b40e4946
Studdyy <44733696+Studdyy@users.noreply.github.com>
2018-11-03 22:04:53 -0700

    Initial Commit

---
+++ Ranker.py
@@ -0,0 +1,18 @@
+
+""" Reddit Recommends"""
+
+import Reccomendation
+
+def remove_duplicates(RecDic): # Filters list of any reoccurung links.
+	Recs = RecDic.values
+
+
+
+def rank_products(list_of_instacnes) # Ranks products according to score.
+	pass
+
+links = ["https://www.reddit.com/r/MechanicalKeyboards/comments/7js58d/what_mechanical_keyboard_should_i_buy/",
+         "https://www.reddit.com/r/MechanicalKeyboards/comments/8ekjay/best_mechanical_keyboard_100200/",
+         "https://www.reddit.com/r/MechanicalKeyboards/comments/8lk5nh/mechanical_keyboard_suggestions/"]
+
+reccs = LinkParser(links)
